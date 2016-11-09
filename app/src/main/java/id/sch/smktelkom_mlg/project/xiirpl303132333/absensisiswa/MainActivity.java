6c0e5be09b551f0fec1c4d81eedf2d63158bae4f
Novalia <Novaliapopy39@gmail.com>
2016-11-09 13:26:21 +0700

    form login

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xiirpl303132333.absensisiswa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        findViewById(R.id.buttonlogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
 }
+}
+
