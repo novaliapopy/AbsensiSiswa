5436ab81b04b56168bc8688af422eb0f12f0ab9b
Enrico_Didan <ricodidan@gmail.com>
2016-11-09 17:32:23 +0700

    Firebase Test SignUp Success.

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonlogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Login.class));
+                startActivity(new Intent(MainActivity.this, activity_signup.class));
             }
         });
 
