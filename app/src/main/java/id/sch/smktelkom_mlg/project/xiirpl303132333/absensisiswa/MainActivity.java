0a897aa164c9d1af6c988422303f01e023a45e9f
Novalia <Novaliapopy39@gmail.com>
2016-11-09 14:05:07 +0700

    form signup

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.buttonSignup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, signup.class));
+            }
+        });
 }
 }
 
