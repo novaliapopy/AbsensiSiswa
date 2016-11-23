790d0631150eea404631728f7d4dc046fe7ff0ba
Enrico_Didan <ricodidan@gmail.com>
2016-11-23 11:59:05 +0700

    Update Signup Form.

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/activity_signup.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/absensisiswa/activity_signup.java
@@ -6,8 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,7 +21,7 @@ public class activity_signup extends AppCompatActivity implements View.OnClickLi
     EditText editTextEmail;
     EditText editTextPassword;
     //Button buttonSignup;
-    Button buttonSignup;
+    ImageButton buttonSignup;
     ProgressDialog progressDialog;
 
 
@@ -40,7 +40,7 @@ public class activity_signup extends AppCompatActivity implements View.OnClickLi
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
-        buttonSignup = (Button) findViewById(R.id.buttonSignup);
+        buttonSignup = (ImageButton) findViewById(R.id.buttonSignup);
 
         progressDialog = new ProgressDialog(this);
 
